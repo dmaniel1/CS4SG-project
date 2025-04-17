f1d5f654015b769101c82d6f874f66056793c7d2
Daniel <119364597+dmaniel1@users.noreply.github.com>
2025-04-16 19:56:41 -0500

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,30 @@
+public class Room {
+    private int number;
+    private char nearestStairwell;
+    private int floor;
+    private boolean odd;
+    public Room(int num, char stair, int curFloor, boolean o) {
+        number = num;
+        nearestStairwell = stair;
+        floor = curFloor;
+        odd = o;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public char getStair() {
+        return nearestStairwell;
+    }
+    public int getFloor() {
+        return floor;
+    }
+    public boolean getOOrE() {
+        return odd;
+    }
+    public void updateFloor(int newFloor) {
+        floor = newFloor;
+    }
+    public void updateStairs(char stairwa) {
+        nearestStairwell = stairwa;
+    }
+}
